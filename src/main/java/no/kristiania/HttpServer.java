05e63ed9b25a79ddb37fba0a2d4ab88e424849f9
hanadbruno <hanad1997@hotmail.no>
2021-11-01 19:51:20 +0100

    Questions class

--- src/main/java/no/kristiania/HttpServer.java
+++ src/main/java/no/kristiania/HttpServer.java
@@ -1,4 +1,17 @@
 package no.kristiania;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class HttpServer {
+
 
 }
---
+++ src/main/java/no/kristiania/Questions.java
@@ -0,0 +1,10 @@
+package no.kristiania;
+
+public class Questions {
+private int id;
+private String questionTitle;
+private String questionText;
+private String questionOption;
+
+
+}
